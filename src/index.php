45513385531b9ef6d5db89adaee14079b38c52df
pieroit <savastanos@libero.it>
2014-11-18 16:00:47 +0100

    first

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SLT Jukebox</title>
+        <meta name="viewport" content="width=device-width">        
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <div id="main">
+            <div id="header">
+            
+            <div id="sculpture"></div>
+          
+        </div>
+
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="js/jukebox.js"></script>
+    </body>
+</html>
